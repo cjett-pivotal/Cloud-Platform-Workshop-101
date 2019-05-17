1663f7f63ae8528cff1d75a289f03c9a6b047814
Cory Jett <cjett@pivotal.io>
2019-05-17 09:17:50 -0500

    changes org name

--- cleanup_workshop.sh
+++ cleanup_workshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-USERS="$(cf space-users home workshop | sed -e '1,/DEVELOPER/d; /AUDITOR/,$d'|grep user)"
+USERS="$(cf space-users DNA workshop | sed -e '1,/DEVELOPER/d; /AUDITOR/,$d'|grep user)"
 for USER in $USERS
 do
     echo $USER
     cf delete-user ${USER} -f
     ((counter++))
 done
-cf delete-space workshop -f
+cf delete-space workshop  -o DNA -f
